25b4616534624b090785d35a4538c4bf461ab85b
derekbkk <bkktrending@gmail.com>
2023-10-29 07:51:32 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7fa7ac16-f021-4ba4-ba41-12a14ddff79a';
+export const MODAL_CLOSE_SEC = 2.5;
